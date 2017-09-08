013d5caf14c0e35306dee2f4950b74ec48188585
WesleyFakkeldij <wesley.fakkeldij@student.hu.nl>
2017-09-08 15:55:19 +0200

    Opdracht 1_3

---
+++ Les 2/Opdracht.py
@@ -0,0 +1,20 @@
+# 1. Ken de waarde 6 toe aan variabele a, en waarde 7 aan variabele b.
+# 2. Ken aan variabele c als waarde het gemiddelde van a en b toe.
+# 3. Ken aan variabele inventaris de een lijst van strings toe: ‘papier’, ‘nietjes’, en ‘pennen’.
+# 4. Ken aan variabelen voornaam, tussenvoegsel en achternaam je eigen naamgegevens toe.
+# 5. Ken aan variabele mijnnaam de variabelen van opdracht 4 (met spaties er tussen) toe.
+
+a = 6
+b = 7
+c = (a+b)/2
+print (c)
+
+voorNaam = 'Wesley'
+tussenVoegsel = ''
+achterNaam = 'Fakkeldij'
+blankSpace = ' '
+
+mijnNaam = voorNaam + tussenVoegsel + blankSpace + achterNaam
+
+print (mijnNaam)
+
